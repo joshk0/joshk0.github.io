69d7a43e4cbd0c7d7d4668aac1d21efa65b61bea
Joshua Kwan <joshua.kwan@liveramp.com>
2018-02-19 10:00:04 -0500

    Initial commit

---
+++ resume.js
@@ -0,0 +1,8 @@
+$.ajax({
+  url: 'https://circleci.com/api/v1.1/project/github/joshk0/resume/latest/artifacts',
+  dataType: 'json'
+}).done(function(data) {
+  $.ready(function() {
+    $('#resume-link').attr('href', data[0].url);
+  });
+})
